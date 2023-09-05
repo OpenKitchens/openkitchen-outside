0d4461a88129574e37d526c1f6b50027a705b42e
Open Kitchens <openkitchens.moyasi@gmail.com>
2023-09-05 22:34:18 +0900

    gg

--- src/components/globals/Header.vue
+++ src/components/globals/Header.vue
@@ -91,7 +91,7 @@ const submitForm = () => {
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#" style="font-weight: bold;">Openkitchen</a>
+      <a class="navbar-brand" href="/" style="font-weight: bold;">Openkitchen</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
